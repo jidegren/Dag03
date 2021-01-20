b9d4648a944ad778c4f64e29f1b4027eaa28e91a
Robin Jidegren <robin_jide@hotmail.com>
2021-01-20 10:53:02 +0100

    Initial commit

---
+++ Dag03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dag03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
